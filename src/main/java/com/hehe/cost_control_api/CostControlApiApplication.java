650c204ed835e0e2d3609e2fdb7d1e9cdd1dd482
Mudien-UB <mudienub@gmail.com>
2025-06-14 17:49:03 +0700

    initial projek

---
+++ src/main/java/com/hehe/cost_control_api/CostControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.hehe.cost_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hehe/cost_control_api/CostControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehe.cost_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
